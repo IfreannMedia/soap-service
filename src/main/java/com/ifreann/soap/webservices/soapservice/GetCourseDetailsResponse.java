204eb5ff540d46d1d617cbee7dec0833972a9fb0
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-11-05 13:50:34 +0100

    generated java objects from xsd using JAX2B

---
+++ src/main/java/com/ifreann/soap/webservices/soapservice/GetCourseDetailsResponse.java
@@ -0,0 +1,64 @@
+
+package com.ifreann.soap.webservices.soapservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="CourseDetails" type="{http://ifreann.com}CourseDetails"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "courseDetails"
+})
+@XmlRootElement(name = "GetCourseDetailsResponse", namespace = "http://ifreann.com")
+public class GetCourseDetailsResponse {
+
+    @XmlElement(name = "CourseDetails", namespace = "http://ifreann.com", required = true)
+    protected CourseDetails courseDetails;
+
+    /**
+     * Gets the value of the courseDetails property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link CourseDetails }
+     *     
+     */
+    public CourseDetails getCourseDetails() {
+        return courseDetails;
+    }
+
+    /**
+     * Sets the value of the courseDetails property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link CourseDetails }
+     *     
+     */
+    public void setCourseDetails(CourseDetails value) {
+        this.courseDetails = value;
+    }
+
+}
---
+++ src/main/java/com/ifreann/soap/webservices/soapservice/ObjectFactory.java
@@ -0,0 +1,56 @@
+
+package com.ifreann.soap.webservices.soapservice;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.ifreann.soap.webservices.soapservice package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.ifreann.soap.webservices.soapservice
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetCourseDetailsRequest }
+     * 
+     */
+    public GetCourseDetailsRequest createGetCourseDetailsRequest() {
+        return new GetCourseDetailsRequest();
+    }
+
+    /**
+     * Create an instance of {@link GetCourseDetailsResponse }
+     * 
+     */
+    public GetCourseDetailsResponse createGetCourseDetailsResponse() {
+        return new GetCourseDetailsResponse();
+    }
+
+    /**
+     * Create an instance of {@link CourseDetails }
+     * 
+     */
+    public CourseDetails createCourseDetails() {
+        return new CourseDetails();
+    }
+
+}
